083ee16cc74849015850d9caadff11c553e28419
jwz <1109704065@qq.com>
2021-01-16 18:40:24 +0800

    fix code

--- paperadmin/chunk-2d0b59f4.1.0.8.1610792587145.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b59f4"],{"1a4b":function(n,t,e){"use strict";e.r(t);var u=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div")},o=[],c={mounted:function(){this.$router.go(-1)}},r=c,i=e("2877"),l=Object(i["a"])(r,u,o,!1,null,null,null);t["default"]=l.exports}}]);
